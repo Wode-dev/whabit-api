f8868508b4480fc3da9784c43fdaa54322b7486c
Albertto Castro <albertto.programmer@gmail.com>
2020-07-28 15:17:47 -0300

    Cria 'model' de habitos Controlador, factory, model e migration

---
+++ app/Http/Controllers/HabitController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Habit;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class HabitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Habit::all()->toArray();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Habit::create($request->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Habit $habit)
+    {
+        return $habit->toArray();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Habit $habit)
+    {
+        $habit->update($request->toArray());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Habit  $habit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Habit $habit)
+    {
+        $habit->delete();
+    }
+}
