908c4ffd9aeb5d5ddefb1aebbe1ef6c22acb0f9e
Albertto Castro <albertto.programmer@gmail.com>
2020-07-30 08:45:26 -0300

    Habita o CORS

--- app/Http/Resources/Habit.php
+++ app/Http/Resources/Habit.php
@@ -14,6 +14,12 @@ class Habit extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'data' => [
+                'accomplishment_amount' => $this->accomplishments->count()
+            ]
+        ];
     }
 }
