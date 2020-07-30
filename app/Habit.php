8e2ac890eace1e61ebbb07930e8101e857dc8ffd
Albertto Castro <albertto.programmer@gmail.com>
2020-07-30 08:46:18 -0300

    Habilita name como fillable

--- app/Habit.php
+++ app/Habit.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Habit extends Model
 {
+    protected $fillable =[
+        "name"
+    ];
     public function accomplishments()
     {
         return $this->hasMany('App\Accomplishment', 'habit_id', 'id');
