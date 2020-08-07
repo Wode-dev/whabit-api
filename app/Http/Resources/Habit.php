3722b60dccc27a2a1f9909ca9220c98e1506d3bc
Albertto Castro <albertto.programmer@gmail.com>
2020-08-07 17:12:54 -0300

    Adiciona ultimos accomplishments para o resource

--- app/Http/Resources/Habit.php
+++ app/Http/Resources/Habit.php
@@ -14,12 +14,22 @@ class Habit extends JsonResource
      */
     public function toArray($request)
     {
+        // Last five accomplishments
+        $lastAccomplishments = $this->accomplishments->count() > 0 ? $this
+                ->accomplishments
+                ->toQuery()
+                ->where(
+                    'date',
+                    '>=',
+                    now()->subDays(5)->toDateTimeString()
+                )->get() : [];
         return [
             'id' => $this->id,
             'name' => $this->name,
             'data' => [
                 'accomplishment_amount' => $this->accomplishments->count()
-            ]
+            ],
+            'accomplishments' => $lastAccomplishments,
         ];
     }
 }
