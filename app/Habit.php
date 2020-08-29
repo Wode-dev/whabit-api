86e3114fac25f7977e1b9e816230af18f791615c
Albertto Castro <albertto.programmer@gmail.com>
2020-08-29 15:27:52 -0300

    Add user reference to habit

--- app/Habit.php
+++ app/Habit.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Habit extends Model
 {
-    protected $fillable =[
+    protected $fillable = [
         "name"
     ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User', 'user_id', 'id');
+    }
+
     public function accomplishments()
     {
         return $this->hasMany('App\Accomplishment', 'habit_id', 'id');
